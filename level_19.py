73d4c8b061b0427ef6efd47c9a22e8e7b051ffd3
Trevor Bernard <Tbernard260@gmail.com>
2022-08-04 15:27:54 -0400

    added problem 19

---
+++ level_19.py
@@ -0,0 +1,7 @@
+import datetime
+sunday_counter = 0
+for year in range(1901,2001): # range of years we need to check
+    for month in range(1,13): # range of months in a year
+        if datetime.datetime(year,month,1).weekday() == 6: # if the 1st of the month is on a sunday then we add it to the counter
+            sunday_counter += 1
+print(sunday_counter)
